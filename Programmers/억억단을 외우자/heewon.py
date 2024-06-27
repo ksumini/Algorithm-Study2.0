3f11ce83eb49a53911e8ee81d1bb5204435a530b
HeewonKwak <hewo1217@gmail.com>
2024-06-27 17:41:41 +0900

    Update 억억단을 외우자 heewon.py - 최적화

--- Programmers/억억단을 외우자/heewon.py
+++ Programmers/억억단을 외우자/heewon.py
@@ -27,22 +27,20 @@ def solution(e, starts):
     division_cnt = [0] * (e + 1)
 
     # 각 수의 약수 개수를 계산하는 과정
-    for i in range(1, e + 1):
-        for j in range(i, e + 1, i):
-            division_cnt[j] += 1  # i는 j의 약수이므로 division_cnt[j] 증가
+    for i in range(1, int(e**0.5)+1):
+        division_cnt[i*i] += 1
+        for j in range(i * (i + 1), e + 1, i):
+            division_cnt[j] += 2    # 배수 값을 미리 계산
 
-    # 최대 약수 개수를 가지는 숫자 저장을 위한 리스트 초기화 (마지막 값부터 시작)
     max_idx = [0] * (e + 1)
     max_idx[e] = e
-    max_val = division_cnt[e]  # 시작에서 최대 약수 개수 (마지막 값)
 
     # 역순으로 최대 약수 개수, index 정보 업데이트
-    for k in range(e - 1, 0, -1):
-        if max_val > division_cnt[k]:
+    for k in range(e - 1, min(starts) - 1, -1):
+        if division_cnt[max_idx[k+1]] > division_cnt[k]:
             max_idx[k] = max_idx[k + 1]  # 이전 인덱스의 값 유지 (같거나 더 큰 값)
         else:
-            max_idx[k] = k  # 현재 값이 index 갱신
-            max_val = division_cnt[k]  # 최대 약수 개수 갱신
+            max_idx[k] = k  # 현재 값이 index 갱신개수 갱신
 
     # starts 리스트에 있는 수들의 최대 약수 개수를 가진 수의 인덱스 반환
     return [max_idx[s] for s in starts]
