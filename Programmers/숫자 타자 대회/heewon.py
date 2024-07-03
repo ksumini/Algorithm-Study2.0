df6cd83a3eae00d0c5b9ba2271888640f1c32909
HeewonKwak <hewo1217@gmail.com>
2024-07-04 01:43:57 +0900

    Update 숫자 타자 대회 heewon.py

--- Programmers/숫자 타자 대회/heewon.py
+++ Programmers/숫자 타자 대회/heewon.py
@@ -1,3 +1,26 @@
+'''
+## 함수 설명
+- `get_weight`: 두 숫자 버튼 간의 거리에 해당하는 가중치 값
+- `square_dist`: 두 좌표 간의 제곱 거리 값
+
+## 접근 방식
+- 1st method - fail
+    - 웬지 아닐 것 같지만 떠오르는 방법이 없어서 그리디로 접근
+    - 최소 가중치를 가지는 번호로 이동
+    - 동일한 가중치면 저장하고 나중에 계산
+    - 반례를 찾음 '7240'
+    - 느낀점
+        - 일단 구현할 생각 보다는 반례를 생각해 보고 구현 하자(구현 시간 >> 반례 찾는 시간)
+- 2nd method - 힌트 참고(`n이 10만이지만, 타이핑하기전의 엄지의 위치는 0 1 ~ 9 8 까지 100개가 되지 않습니다`)
+    - 각각의 step마다 가능한 손가락들의 조합을 저장한다.
+    - 겹치는 조합들은 최솟값으로 저장
+    - 손가락이 동일한 숫자를 안 누르게 조심
+
+## 추가 정보
+- 시간: 2 hour 이하
+- 힌트: `Tip`
+'''
+
 def solution(numbers: str) -> int:
     """
     입력된 문자열 `numbers`에서 가장 적은 손가락 움직임으로 텍스트를 입력하는 방법을 찾아 최소 움직임 값을 반환하는 함수입니다.
@@ -10,10 +33,10 @@ def solution(numbers: str) -> int:
     """
 
     prev_weight = {'46': 0} # 초기의 손가락의 가중치 값
-    prev_fingers = {'46'}   # 초기의 손가락 위치
+    prev_fingers = ['46']   # 초기의 손가락 위치
 
     for num in numbers:
-        current_fingers = set() # 현재의 손가락 위치들
+        current_fingers = [] # 현재의 손가락 위치들
         current_weight = {}     # 현재의 손가락 위치의 가중치 값
 
         for fingers in prev_fingers:    # 이전의 손가락 위치들
@@ -26,7 +49,7 @@ def solution(numbers: str) -> int:
                     current_weight[new_fingers] = min(current_weight[new_fingers], weight + get_weight(left, num))  # 최솟값으로 수정
                 else:   # 첫 손가락 조합
                     current_weight[new_fingers] = weight + get_weight(left, num)
-                    current_fingers.add(new_fingers)    # 손가락 조합 추가 
+                    current_fingers.append(new_fingers)    # 손가락 조합 추가 
 
             if num != left:     # 손가락 중복 피하기 위한 조건
                 new_fingers = left + num    # 새로운 손가락 조합
@@ -34,7 +57,7 @@ def solution(numbers: str) -> int:
                     current_weight[new_fingers] = min(current_weight[new_fingers], weight + get_weight(right, num)) # 최솟값으로 수정
                 else:   # 첫 손가락 조합
                     current_weight[new_fingers] = weight + get_weight(right, num)
-                    current_fingers.add(new_fingers)    # 손가락 조합 추가 
+                    current_fingers.append(new_fingers)    # 손가락 조합 추가 
 
         prev_fingers = current_fingers  # 이전 조합 갱신
         prev_weight = current_weight    # 이전 가중치 정보 갱신
