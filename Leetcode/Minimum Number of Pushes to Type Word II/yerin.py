ea357c598d8bd2d9b1b970ef52fba68f4098416b
ssun <56533266+blaire-pi@users.noreply.github.com>
2024-08-13 10:54:32 +0900

    Create Minimum Number of Pushes to Type Word II yerin.py

---
+++ Leetcode/Minimum Number of Pushes to Type Word II/yerin.py
@@ -0,0 +1,25 @@
+'''
+소문자 str
+'''
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        numpads = defaultdict(int)
+        numbers = [2, 3, 4, 5, 6, 7, 8, 9]
+        i = 0
+        answer = 0
+
+        # 문자 카운트
+        counter_alphabet = Counter(word)
+        # 개수가 적은 순으로 숫자 패드에 매핑
+        counter_alphabet_sort = sorted(counter_alphabet.items(), key=lambda x: x[1], reverse= True)
+
+        for alph, cnt in counter_alphabet_sort:
+            numpads[numbers[i]] += 1
+            answer += cnt * numpads[numbers[i]]
+            i = (i + 1) % len(numbers)
+
+        return answer
+
