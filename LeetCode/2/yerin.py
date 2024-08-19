139242749ed1b6f8ee399d5b6563e17eb25f5d92
ssun <56533266+blaire-pi@users.noreply.github.com>
2024-08-20 01:15:54 +0900

    Update 2 yerin.py

--- LeetCode/2/yerin.py
+++ LeetCode/2/yerin.py
@@ -8,28 +8,22 @@ class ListNode:
 거꾸로 저장됨
 하나의 노드에 하나의 숫자
 '''
-
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # 결과 리스트의 첫 노드 생성
-        node = ListNode(0)
-        current = node  # 결과 리스트의 현재 노드를 가리킴
-        up = 0  # 자리 올림수
+        node = head = ListNode(0)
+        extra = 0  # 자리 올림수 초기화
 
-        while l1 or l2 or up:
-            total = up  # 현재 합계에 자리 올림수를 더함
+        while l1 or l2 or extra:
+            total = extra  # 자리 올림수 추가
             if l1:
                 total += l1.val
-                l1 = l1.next  # l1의 다음 노드로 이동
+                l1 = l1.next
             if l2:
                 total += l2.val
-                l2 = l2.next  # l2의 다음 노드로 이동
+                l2 = l2.next
 
-            up, current.val = divmod(total, 10)  # 자리 올림수와 현재 노드의 값 계산
+            extra, val = divmod(total, 10)  # 올림수와 현재 노드의 값 계산
+            head.next = ListNode(val)
+            head = head.next  # 현재 노드 이동
 
-            if l1 or l2 or up:  # 다음 노드가 필요한 경우에만 새 노드 생성
-                current.next = ListNode(0)
-                current = current.next  # 다음 노드로 이동
-
-        return node  # 결과 리스트의 첫 노드를 반환
+        return node.next
