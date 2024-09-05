db2b36bd8dd0e7ec91fd3b16f7a4e4c0149fb3c4
HeewonKwak <hewo1217@gmail.com>
2024-09-05 17:03:35 +0900

    Update 452 heewon.py

--- LeetCode/452/heewon.py
+++ LeetCode/452/heewon.py
@@ -1,3 +1,4 @@
+# 초기 코드
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         answer = 1  # 첫 화살
@@ -12,3 +13,23 @@ class Solution:
                 answer += 1
                 
         return answer
+    
+# 코드 수정 + 입출력 방식 수정
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        answer = 1
+        sorted_points = sorted(points, key=lambda x: x[1])
+        now = sorted_points[0][1]
+
+        for x1, x2 in sorted_points:
+            if x1 > now:
+                now = x2
+                answer += 1
+                
+        return answer
+
+with open("user.out", "w") as f:
+    for points in map(loads, stdin):
+        s = Solution()
+        f.write(f"{s.findMinArrowShots(points)}\n")
+exit(0)
