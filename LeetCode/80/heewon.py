ea6a96a1dac7cc7edafdf95157c79e3f4fc0cedd
HeewonKwak <hewo1217@gmail.com>
2024-10-07 22:26:41 +0900

    Update 80 heewon.py

--- LeetCode/80/heewon.py
+++ LeetCode/80/heewon.py
@@ -1,16 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        check = False
-        before_num = -10^4 - 1
-        exp_len = 0
-        for num in nums:
-            if num != before_num:
-                nums[exp_len] = num
-                exp_len += 1
-                before_num = num
-                check = True
-            elif check:
-                check = False
+        exp_len = 2  # 고유한 숫자를 저장할 위치
+        for num in nums[2:]:
+            # 현재 숫자가 2개 전 숫자와 다를 경우
+            if num != nums[exp_len - 2]:
                 nums[exp_len] = num
                 exp_len += 1
         return exp_len
