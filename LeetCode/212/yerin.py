dd1435b5d44447d4f3997b76dc89d3dcd64d53a2
Riin♣︎ <56533266+blaire-pi@users.noreply.github.com>
2024-10-31 12:58:16 +0900

    Update yerin.py

--- LeetCode/212/yerin.py
+++ LeetCode/212/yerin.py
@@ -10,7 +10,7 @@ class Solution:
         self.m, self.n = None, None
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        self.trie = self._build_trie(words)
+        self.trie = self.build_trie(words)
         self.board = board
         self.found_words = []
         self.m, self.n = len(board), len(board[0])
@@ -19,11 +19,11 @@ class Solution:
         for i in range(self.m):
             for j in range(self.n):
                 if self.board[i][j] in self.trie:
-                    self._dfs(i, j, self.trie)
+                    self.dfs(i, j, self.trie)
 
         return self.found_words
 
-    def _build_trie(self, words: List[str]) -> dict:
+    def build_trie(self, words: List[str]) -> dict:
         """단어 목록으로부터 Trie 구조를 생성합니다."""
         trie = {}
         for word in words:
@@ -33,7 +33,7 @@ class Solution:
             node[self.WORD_KEY] = word
         return trie
 
-    def _dfs(self, i: int, j: int, node: dict) -> None:
+    def dfs(self, i: int, j: int, node: dict) -> None:
         letter = self.board[i][j]
         curr_node = node[letter]
 
@@ -49,7 +49,7 @@ class Solution:
         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             ni, nj = i + di, j + dj
             if 0 <= ni < self.m and 0 <= nj < self.n and self.board[ni][nj] in curr_node:
-                self._dfs(ni, nj, curr_node)
+                self.dfs(ni, nj, curr_node)
 
         # 방문 처리 해제
         self.board[i][j] = letter
