dd620f8da499045c577dab52b17be4877e899690
HeewonKwak <hewo1217@gmail.com>
2024-12-02 17:13:08 +0900

    Update 198 heewon.py

--- LeetCode/198/heewon.py
+++ LeetCode/198/heewon.py
@@ -3,5 +3,5 @@ class Solution:
         houses = [nums[0], 0]   # [마지막 집이 털린 경우, 마지막 집이 안 털린 경우]
 
         for i in range(1, len(nums)):
-            houses[0], houses[1] = max(houses[1] + nums[i], houses[0]), max(houses[1], houses[0])
+            houses[0], houses[1] = max(houses[1] + nums[i], houses[0]), houses[0]
         return max(houses)
