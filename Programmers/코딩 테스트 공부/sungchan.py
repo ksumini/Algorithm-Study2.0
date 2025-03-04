f4e402261ea45573d40e2dd141b357967cccb9ac
sungchan1 <sungchan254@naver.com>
2025-03-04 18:06:46 +0900

    Create 코딩 테스트 공부 sungchan.py

--- Programmers/코딩 테스트 공부/sungchan.py
+++ Programmers/코딩 테스트 공부/sungchan.py
@@ -8,23 +8,22 @@ def solution(alp, cop, problems):
         goal_alp = max(goal_alp, alp_req)
         goal_cop = max(goal_cop, cop_req)
 
-    dp = [[float("inf")] * (goal_cop + 1) for _ in range(goal_alp + 1)]
+    dp = [[float("inf")] * (goal_cop + 2) for _ in range(goal_alp + 2)]
 
-    for i in range(alp + 1):
-        for j in range(cop + 1):
-            dp[i][j] = 0
+    dp[alp][cop] = 0
 
-    result = float("inf")
-    for i in range(goal_alp+1):
-        for j in range(goal_cop+1):
+    print(alp, goal_alp, cop, goal_cop)
+    for i in range(alp, goal_alp + 1):
+        for j in range(cop, goal_cop + 1):
             # 트레이닝 해서 올리는 방법
-            dp[i][j] = min(dp[i][j-1]+1, dp[i-1][j])
-            #문제 풀어서 올리는 방법
+            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
+            dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
+            # 문제 풀어서 올리는 방법
             for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-                if alp_rwd > i or cop_rwd > j:
+                if i < alp_req or j < cop_req:
                     continue
+                alp_up = min(goal_alp, i + alp_rwd)
+                cop_up = min(goal_cop, j + cop_rwd)
+                dp[alp_up][cop_up] = min(dp[alp_up][cop_up], dp[i][j] + cost)
 
-                if i-alp_rwd < alp_req or j-cop_rwd < cop_req:
-                    continue
-                dp[i][j] = min(dp[i-alp_rwd][j-cop_rwd]+cost, dp[i][j])
     return dp[goal_alp][goal_cop]
