c53d8a971985071c0365df33a28806fe97f45df1
ssun <56533266+blaire-pi@users.noreply.github.com>
2025-03-27 19:30:19 +0900

    Create 21279 yerin.py

---
+++ BaekJoon/21279/yerin.py
@@ -0,0 +1,29 @@
+from collections import deque
+import heapq
+import sys
+
+
+input = sys.stdin.readline
+n, c = map(int, input().split())
+gems = [list(map(int, input().split())) for _ in range(n)]
+gems.sort()
+
+info = []
+heapq.heapify(info)
+
+total = 0
+ans = 0
+
+for w, h, v in gems:
+    c -= 1
+    total += v
+    heapq.heappush(info, [-h, v])
+    while c < 0:
+        _, prev_v = heapq.heappop(info)
+        c += 1
+        total -= prev_v
+
+    ans = max(ans, total)
+
+print(ans)
+
