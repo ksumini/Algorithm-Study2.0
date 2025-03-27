bf2b795fbf0df962ed1f4dda1c20b1ffd9717476
sungchan1 <sungchan254@naver.com>
2025-03-27 18:59:42 +0900

    Create 21279 sungchan.py

--- BaekJoon/21279/sungchan.py
+++ BaekJoon/21279/sungchan.py
@@ -1,51 +1,53 @@
 from collections import defaultdict, deque, namedtuple
+from dataclasses import dataclass
 
 Point = namedtuple('Point', ['x', 'y'])
-Mine = namedtuple('Mine', ['value', 'cost'])
+
+@dataclass
+class Mine:
+    value: int
+    cost: int
+
 
 n, cash = map(int, input().split())
-minerals = defaultdict(lambda : defaultdict(int))
+minerals: dict[Point, int] = dict()
+mines: dict[Point, Mine] = dict()
 
-
-max_x = 0
-max_y = 0
+set_x = set()
+set_y = set()
 
 for _ in range(n):
     x, y, value = map(int, input().split())
-    minerals[x][y] = value
-    max_x = max(max_x, x)
-    max_y = max(max_y, y)
-
-mines = [[Mine(0,0) for _ in range(max_y+1)] for _ in range(max_x+1)]
-
-q = deque()
-q.append(Point(0,0))
-result = 0
-
-# BFS (?) 방식으로 탐색 -> 시간 초과
-while q:
-    point = q.popleft()
-    x, y = point.x, point.y
-    if x > max_x or y > max_y:
-        continue
-
-    # 이미 방문
-    if mines[x][y].value != 0:
-        continue
-
-    value = mines[x-1][y].value + mines[x][y-1].value - mines[x-1][y-1].value + minerals[x][y]
-
-    cost = mines[x-1][y].cost + mines[x][y-1].cost - mines[x-1][y-1].cost
-    cost += 1 if minerals[x][y] > 0 else 0
-
-    if cost > cash:
-        value = -float("inf")
-
-    mines[x][y] = Mine(value, cost)
-    result = max(result, value)
-
-    q.append(Point(x+1, y))
-    q.append(Point(x, y+1))
+    point = Point(x, y)
+    minerals[point] = value
+    set_x.add(x)
+    set_y.add(y)
 
 
-print(result)
+for x in set_x:
+    for y in set_y:
+        point = Point(x, y)
+        mines[point] = Mine(0, 0)
+
+for mineral_point, mineral_value in minerals.items():
+    for mine_point in mines.keys():
+        if mine_point.x < mineral_point.x and mineral_point.y < mine_point.y:
+            continue
+
+        if mineral_point.x <= mine_point.x or mineral_point.y <= mine_point.y:
+            mines[mine_point].value += mineral_value
+            mines[mine_point].cost += 1
+
+
+
+max_value = 0
+for mine in mines.values():
+    if mine.cost <= cash:
+        max_value = max(max_value, mine.value)
+
+print(max_value)
+
+
+
+
+
