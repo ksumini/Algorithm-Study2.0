1f59f7b9403c9bd4149bd81fd4db8fc8167dcb58
Gwangseok Han <66068653+c0natus@users.noreply.github.com>
2025-05-11 21:32:12 +0900

    Create 1958 gwangseok.py

---
+++ BaekJoon/1958/gwangseok.py
@@ -0,0 +1,23 @@
+import sys
+read_line = sys.stdin.readline
+
+
+def lcs(str1, str2, str3):
+    dp = [[[0] * (len(str3) + 1) for _ in range(len(str2) + 1)] for __ in range(len(str1) + 1)]
+
+    for i in range(len(str1)):
+        for j in range(len(str2)):
+            for k in range(len(str3)):
+                if str1[i] == str2[j] == str3[k]:
+                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1
+                else:
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1])
+
+    return dp[len(str1)-1][len(str2)-1][len(str3)-1]
+
+# 입력 예제
+num_str = 3
+st = [read_line().strip() for _ in range(num_str)]
+
+# 결과 출력
+print(lcs(*st))
